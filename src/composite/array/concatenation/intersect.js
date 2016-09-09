14b518b62ee88e729d5d7b78f9f6269a3aa2e4b6
ivenmarquardt <info@kongware.net>
2016-09-09 13:13:18 +0200

    Version 0.0.5

--- src/composite/array/concatenation/intersect.js
+++
@@ -1,9 +0,0 @@
-const afrom = require("../construction/afrom");
-const createSet = require("../../../abstract/set/createSet");
-const filter = require("../searching/filter");
-const has = require("../../../abstract/set/has");
-
-module.exports = intersect = ys => xs => {
-  ys = createSet(ys);
-  return filter(x => has(x) (ys)) (afrom(createSet(xs)));
-}
