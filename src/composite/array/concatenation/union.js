d90f33afe183ff8df3e0037bde39e2dbddd56c52
ivenmarquardt <info@kongware.net>
2016-09-09 13:09:49 +0200

    Version 0.0.5

--- src/composite/array/concatenation/union.js
+++ src/composite/array/concatenation/union.js
@@ -1,12 +1,4 @@
+const apply = require("../../../polymorphic/application/apply");
 const concat_ = require("./concat_");
-const createSet = require("../../../abstract/set/createSet");
-const filter = require("../searching/filter");
-const has = require("../../../abstract/set/has");
-const notf = require("../../../polymorphic/negation/notf");
-const unique = require("../normalization/unique");
 
-module.exports = union = ys => xs => {
-  ys = unique(ys);
-  zs = createSet(xs);
-  return concat_(xs) (filter(notf(has(zs))) (ys));
-}
+module.exports = union = unionBy(apply);
---
+++ src/composite/array/concatenation/unionBy_.js
@@ -0,0 +1,4 @@
+const concat_ = require("./concat_");
+const flip = require("../../../polymorphic/argument/flip");
+
+module.exports = unionBy_ = flip(unionBy);
