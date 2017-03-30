390a836fe8d9b723a9ecaf3331132fb84e0e6b1c
ivenmarquardt <info@kongware.net>
2017-03-30 10:22:22 +0200

    Version 0.1.17

--- src/product/tuple/eq.js
+++ src/product/tuple/eq.js
@@ -18,12 +18,12 @@ const len = require("./len");
  */
 
 
-// Object -> (a -> b) -> (a -> b) -> Boolean
+// Eq a => Object -> (a -> b) -> (a -> b) -> Boolean
 const eq = Rep => t2 => t1 => t1(len) === t2(len)
  && t1(x => t2(y => Rep.eq_(x, y)));
 
 
-// Object -> Object -> ((a, b) -> c) -> ((a, b) -> c) -> Boolean
+// Eq a => Object -> Object -> ((a, b) -> c) -> ((a, b) -> c) -> Boolean
 const eq2 = Rep1 => Rep2 => t2 => t1 => t1(len) === t2(len)
  && t1((w, x) => t2((y, z) => Rep1.eq_(w, y) && Rep2.eq_(x, z)));
 
