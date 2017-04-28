f5f03a8961ced28348f65d437d19399bd4b4cde8
ivenmarquardt <info@kongware.net>
2017-04-28 18:13:07 +0200

    Version 0.1.36

--- src/primitive/boolean/Ord.js
+++ src/primitive/boolean/Ord.js
@@ -5,17 +5,37 @@
 
 
 const {compare, compare_} = require("./compare");
+const {lt, lt_} = require("./lt");
+const {lte, lte_} = require("./lte");
+const {gt, gt_} = require("./gt");
+const {gte, gte_} = require("./gte");
+const min = require("./min");
+const max = require("./max");
 
 
 /**
  * @name Order
  * @note total order
  * @type type representative
- * @status unstable
+ * @status stable
  */
 
 
 const Ord = {};
+
+
+Ord.compare = compare;
+Ord.compare_ = compare_;
+Ord.lt = lt;
+Ord.lt_ = lt_;
+Ord.lte = lte;
+Ord.lte_ = lte_;
+Ord.gt = gt;
+Ord.gt_ = gt_;
+Ord.gte = gte;
+Ord.gte_ = gte_;
+Ord.min = min;
+Ord.max = max;
 
 
 // API
