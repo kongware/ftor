67c924b51dee73a7aedbc441fc7e03e42f20d29a
ivenmarquardt <info@kongware.net>
2017-06-17 19:52:05 +0200

    Version 0.2.29

---
+++ src/sum/Exception.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+// dependencies
+
+
+// ...
+
+
+/**
+ * @name Exception
+ * @note monad transformer to model exceptions in a pure environment; Church encoded
+ * @type sum type
+ * @status stable
+ */
+
+
+const Exception = {};
+
+
+// CONSTRUCTORS
+
+
+// ...
+
+
+// API
+
+
+module.exports = {Validation};
---
+++ src/sum/Validation.js
@@ -0,0 +1,30 @@
+"use strict";
+
+
+// dependencies
+
+
+// ...
+
+
+/**
+ * @name Validation
+ * @note like Either but accumulates exceptions; Church encoded
+ * @type sum type
+ * @status stable
+ */
+
+
+const Validation = {};
+
+
+// CONSTRUCTORS
+
+
+// ...
+
+
+// API
+
+
+module.exports = {Validation};
