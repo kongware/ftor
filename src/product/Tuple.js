d98bc715d76c9356f6ca4ab3ceb955a4ead0ead7
ivenmarquardt <info@kongware.net>
2017-06-17 21:05:05 +0200

    Version 0.2.29

--- src/product/Tuple.js
+++ src/product/Tuple.js
@@ -44,19 +44,23 @@ Tuple.eq2 = tx => ty => tx[$Tuple] && ty[$Tuple] && tx((x1, y1) => ty((x2, y2) =
 
 
 // (Setoid a, Setoid b, Setoid c) => (a, b, c) -> (a, b, c) -> Boolean
-Tuple.eq3 = tx => ty => tx[$Tuple] && ty[$Tuple] && tx((x1, y1, z1) => ty((x2, y2, z2) => x1 === x2 && y1 === y2 && z1 === z2));
+Tuple.eq3 = tx => ty =>
+ tx[$Tuple] && ty[$Tuple] && tx((x1, y1, z1) => ty((x2, y2, z2) => x1 === x2 && y1 === y2 && z1 === z2));
 
 
 // (a -> a -> Boolean) -> (a) -> (a) -> Boolean
-Tuple.eqBy = eq => tx => ty => tx[$Tuple] && ty[$Tuple] && tx(x1 => ty(x2 => eq(x1) (x2)));
+Tuple.eqBy = eq => tx => ty =>
+ tx[$Tuple] && ty[$Tuple] && tx(x1 => ty(x2 => eq(x1) (x2)));
 
 
 // (a -> a -> Boolean) -> (b -> b -> Boolean) -> (a, b) -> (a, b) -> Boolean
-Tuple.eqBy2 = (eq1, eq2) => tx => ty => tx[$Tuple] && ty[$Tuple] && tx((x1, y1) => ty((x2, y2) => eq1(x1) (x2) && eq2(y1) (y2)));
+Tuple.eqBy2 = (eq1, eq2) => tx => ty =>
+ tx[$Tuple] && ty[$Tuple] && tx((x1, y1) => ty((x2, y2) => eq1(x1) (x2) && eq2(y1) (y2)));
 
 
 // (a -> a -> Boolean) -> (b -> b -> Boolean) -> (c -> c -> Boolean) -> (a, b, c) -> (a, b, c) -> Boolean
-Tuple.eqBy3 = (eq1, eq2, eq3) => tx => ty => tx[$Tuple] && ty[$Tuple] && tx((x1, y1, z1) => ty((x2, y2, z2) => eq1(x1) (x2) && eq2(y1) (y2) && eq3(z1) (z2)));
+Tuple.eqBy3 = (eq1, eq2, eq3) => tx => ty =>
+ tx[$Tuple] && ty[$Tuple] && tx((x1, y1, z1) => ty((x2, y2, z2) => eq1(x1) (x2) && eq2(y1) (y2) && eq3(z1) (z2)));
 
 
 // ORD
