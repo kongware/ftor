b9a43b8dfbb8275627fb918baafe8ff672dee480
ivenmarquardt <info@kongware.net>
2017-06-20 21:21:25 +0200

    Version 0.2.32

--- src/apConst.js
+++ src/apConst.js
@@ -9,7 +9,7 @@ const {lifta2} = require("./lifta");
 
 
 /**
- * @name const apply
+ * @name apply const
  * @note take both effects but drop the values of the first/second action
  * @type higher order function
  * @status stable
@@ -18,26 +18,26 @@ const {lifta2} = require("./lifta");
   const lifta2 = (map, ap) => f => tx => ty => ap(map(f) (tx)) (ty);
   const K_ = x => y => y;
 
-  const constAp = (map, ap) => lifta2(map, ap) (K_);
+  const apConst = (map, ap) => lifta2(map, ap) (K_);
 
   const map = f => xs => xs.map(x => f(x));
   const ap = fs => xs => fs.reduce((acc, f) => acc.concat(xs.map(x => f(x))), []);
 
-  constAp(map, ap) ([1, 2]) ([3, 4]); // [3, 4, 3, 4]
-  constAp(map, ap) ([]) ([3, 4]); // []
+  apConst(map, ap) ([1, 2]) ([3, 4]); // [3, 4, 3, 4]
+  apConst(map, ap) ([]) ([3, 4]); // []
 
  */
 
 
 // Applicative f => f a -> f b -> f b
-const constAp = (map, ap) => lifta2(map, ap) (K_);
+const apConst = (map, ap) => lifta2(map, ap) (K_);
 
 
 // Applicative f => f a -> f b -> f a
-const constAp_ = (map, ap) => lifta2(map, ap) (K);
+const apConst_ = (map, ap) => lifta2(map, ap) (K);
 
 
 // API
 
 
-module.exports = {constAp, constAp_};
+module.exports = {apConst, apConst_};
--- src/mapConst.js
+++ src/mapConst.js
@@ -17,22 +17,22 @@ const {K} = require("./K");
 
   const B_ = (...fs) => x => fs.reduceRight((acc, f) => f(acc), x);
   const K = x => y => x;
-  const constMap = map => B_(map, K);
+  const mapConst = map => B_(map, K);
 
   const map = f => xs => xs.map(x => f(x));
   const inc = x => x + 1;
 
-  constMap(map) (7) ([1, 2, 3]); // [7, 7, 7]
-  constMap(map) (7) ([]); // []
+  mapConst(map) (7) ([1, 2, 3]); // [7, 7, 7]
+  mapConst(map) (7) ([]); // []
 
  */
 
 
 // Functor f => a -> f b -> f a
-const constMap = map => B_(map, K);
+const mapConst = map => B_(map, K);
 
 
 // API
 
 
-module.exports = constMap;
+module.exports = mapConst;
